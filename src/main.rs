a26a4a88fbf9e3899e26247570867d60fd99b594
Daniel Martins <cabo@0Bx1371>
2019-11-11 02:55:01 -0300

    Implementada a Função para Cadastrar Contatos

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+extern crate rusqlite;
+use rusqlite::{Connection,NO_PARAMS};
+use std::io;
+
+fn main()
+{
+     let cria_banco = Connection::open("contatos.db").unwrap();
+
+     cria_banco.execute(" CREATE TABLE IF NOT EXISTS Contatos (
+          id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+          nome TEXT NOT NULL,
+          email TEXT NOT NULL,
+          telefone VARCHAR(20),
+          dataNascimento VARCHAR(10)
+               )", NO_PARAMS).unwrap();
+     
+//   let nome: String = "Gibson".to_string();
+//   let email: String = "smith@mormail.com".to_string();
+
+     let mut grava_nome = String::new();
+     let mut grava_email = String::new();
+     let mut grava_telefone = String::new();
+     let mut grava_nascimento = String::new();
+
+     println!("\nEntre com o Nome do Contato: ");
+     io::stdin().read_line(&mut grava_nome).expect("Ocorreu um Erro");
+
+     println!("\nInsira o Email: ");
+     io::stdin().read_line(&mut grava_email).expect("Ocorreu um Erro");
+
+     println!("\nInsira o Número de Telefone: ");
+     io::stdin().read_line(&mut grava_telefone).expect("Ocorreu um Erro");
+
+     println!("\nInsira a Data de Nascimento (00/00/0000): ");
+     io::stdin().read_line(&mut grava_nascimento).expect("Ocorreu um Erro");
+
+     println!("\n----Dados Inseridos----");
+     print!("Nome -> {}",grava_nome);
+     print!("Email -> {}",grava_email);
+     print!("Telefone -> {}",grava_telefone);
+     print!("Nascimento -> {}",grava_nascimento);
+
+     cria_banco.execute("INSERT INTO Contatos (nome,email,telefone,dataNascimento) VALUES (?1,?2,?3,?4)",
+             &[&grava_nome, &grava_email,&grava_telefone,&grava_nascimento]).unwrap();
+}
---
+++ target/debug/Teste_Banco.d
@@ -0,0 +1,1 @@
+/home/cabo/Documentos/rust/TesteBanco/target/debug/Teste_Banco: /home/cabo/Documentos/rust/TesteBanco/src/main.rs
